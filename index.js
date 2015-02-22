e1190cb47bf7364fb710e2b664f01edb7cdd5196
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-22 18:36:36 -0500

    0.2.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * rewrite-ext <https://github.com/jonschlinkert/rewrite-ext>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
@@ -11,12 +11,13 @@ var path = require('path');
 var exts = require('ext-map');
 
 module.exports = function rewrite(fp, ext) {
-  var dirname = path.dirname(fp);
   var extname = path.extname(fp);
-  var basename = path.basename(fp, extname);
+  var len = extname.length;
+
   ext = ext || exts[extname] || extname;
-  if (ext[0] !== '.') {
+  if (ext.charAt(0) !== '.') {
     ext = '.' + ext;
   }
-  return path.join(dirname, basename) + ext;
+
+  return fp.slice(0, fp.length - len) + ext;
 };
